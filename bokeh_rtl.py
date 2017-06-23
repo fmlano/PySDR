cca002ebfa8f42fcb7e58cb19feceb04b150a0ff
Marc L <marcll@vt.edu>
2017-06-22 23:49:25 -0400

    added wrapper for bokeh figure object, to hard code stuff that is almost always used

--- bokeh_rtl.py
+++ bokeh_rtl.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import pysdr # our python package
+
 import numpy as np
 import yaml
 import time 
@@ -14,9 +16,7 @@ from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
 from bokeh.embed import autoload_server
 from bokeh.layouts import column, row, gridplot, Spacer, widgetbox
-from bokeh.models import Slider, Dropdown, Select, TextInput
-from bokeh.models import WheelZoomTool, BoxZoomTool, ResetTool, SaveTool # all the tools we want- reference http://bokeh.pydata.org/en/0.10.0/docs/reference/models/tools.html
-from bokeh.plotting import figure
+from bokeh.models import Select, TextInput
 from bokeh.server.server import Server
 from bokeh.themes import Theme
 from bokeh.util.browser import view # utility to Open a browser to view the specified location.
@@ -29,6 +29,8 @@ from multiprocessing import Process, Manager
 fft_size = 512               # output size of fft, the input size is the samples_per_batch
 waterfall_samples = 100      # number of rows of the waterfall
 samples_per_batch = 256*1024 # num of samples that we process at a time
+samples_in_time_plots = 500  # should be less than samples_per_batch
+
 
 # RTL-SDR stuff (including how to install the driver and wrapper)
 #    git clone https://github.com/osmocom/rtl-sdr.git
@@ -47,8 +49,8 @@ manager = Manager()
 shared_buffer = manager.dict() # there is also an option to use a list
 shared_buffer['waterfall'] = np.ones((waterfall_samples, fft_size))*-100.0 # waterfall buffer
 shared_buffer['psd'] = np.zeros(fft_size) # PSD buffer
-shared_buffer['i'] = np.zeros(fft_size)   # I buffer (time domain)
-shared_buffer['q'] = np.zeros(fft_size)   # Q buffer (time domain)
+shared_buffer['i'] = np.zeros(samples_in_time_plots)   # I buffer (time domain)
+shared_buffer['q'] = np.zeros(samples_in_time_plots)   # Q buffer (time domain)
 shared_buffer['stop-signal'] = False      # used to signal RTL to stop (when it goes true)
 
 # Function that processes each batch of samples that comes in (currently, all DSP goes here)
@@ -60,8 +62,8 @@ def process_samples(samples, rtlsdr_obj):
     waterfall[-1,:] = PSD # fill last row with new fft results
     shared_buffer['waterfall'] = waterfall # you have to copy it back into the manager_list
     shared_buffer['psd'] = PSD # overwrites whatever was in psd buffer, so that the GUI uses the most recent one when it goes to refresh itself
-    shared_buffer['i'] = np.real(samples[0:fft_size]) # i buffer
-    shared_buffer['q'] = np.imag(samples[0:fft_size]) # q buffer
+    shared_buffer['i'] = np.real(samples[0:samples_in_time_plots]) # i buffer
+    shared_buffer['q'] = np.imag(samples[0:samples_in_time_plots]) # q buffer
     # if the change-gain or change-freq callback function signaled STOP then we need to cancel the async read
     if shared_buffer['stop-signal'] == True:
         sdr.cancel_read_async() # needs to be called from this function, so we use the shared memory to send a signal
@@ -81,68 +83,38 @@ p.start()
 # This is the Bokeh "document"
 def main_doc(doc):
     # Frequncy Sink (line plot)
-    fft_plot = figure(plot_width=300, # this is more for the ratio, because we have auto-width scaling
-                      plot_height=200,
-                      y_range=(-70, -10), # dB 
-                      y_axis_label='PSD [dB]',
-                      x_axis_label='Freq [MHz]',
-                      tools=[BoxZoomTool(), WheelZoomTool(dimensions='height'), ResetTool(), SaveTool()],
-                      title="Frequency Sink")  # use min_border=30 to add padding between plots
-    fft_plot.toolbar.active_scroll = fft_plot.toolbar.tools[1] # sets wheel zoom active by default (tools[1] is the wheelzoom)
-    fft_plot.toolbar.logo=None # hides stupid bokeh logo
-    fft_line = fft_plot.line(range(fft_size), range(fft_size), color="aqua", line_width=1) 
-    fft_line.data_source.data['x'] = (np.linspace(-sdr.sample_rate/2.0, sdr.sample_rate/2.0, fft_size) + sdr.center_freq)/1e6 
+    fft_plot = pysdr.base_plot('Freq [MHz]', 'PSD [dB]', 'Frequency Sink', disable_horizontal_zooming=True) 
+    f = (np.linspace(-sdr.sample_rate/2.0, sdr.sample_rate/2.0, fft_size) + sdr.center_freq)/1e6
+    fft_line = fft_plot.line(f, np.zeros(len(f)), color="aqua", line_width=1) # set x values but use dummy values for y
     
     # Time Sink (line plot)
-    time_plot = figure(plot_width=300, # this is more for the ratio, because we have auto-width scaling
-                       plot_height=200,
-                       y_range=(-1.0, 1.0), # amplitude 
-                       y_axis_label=' ',
-                       x_axis_label='Time [ms]',
-                       tools=[BoxZoomTool(), WheelZoomTool(dimensions='height'), ResetTool(), SaveTool()],
-                       title="Time Sink")
-    time_plot.toolbar.active_scroll = time_plot.toolbar.tools[1] # sets wheel zoom active by default (tools[1] is the wheelzoom)
-    time_plot.toolbar.logo=None # hides stupid bokeh logo
-    samples_to_plot = fft_size # to make it easier
-    timeI_line = time_plot.line(range(samples_to_plot), range(samples_to_plot), color="aqua", line_width=1) 
-    timeQ_line = time_plot.line(range(samples_to_plot), range(samples_to_plot), color="red", line_width=1) 
-    timeI_line.data_source.data['x'] = np.linspace(0.0, samples_to_plot / sdr.sample_rate, samples_to_plot) * 1e3 # in ms
-    timeQ_line.data_source.data['x'] = timeI_line.data_source.data['x'] # same x for both lines
+    time_plot = pysdr.base_plot('Time [ms]', ' ', 'Time Sink', disable_horizontal_zooming=True) 
+    t = np.linspace(0.0, samples_in_time_plots / sdr.sample_rate, samples_in_time_plots) * 1e3 # in ms
+    timeI_line = time_plot.line(t, np.zeros(len(t)), color="aqua", line_width=1) # set x values but use dummy values for y
+    timeQ_line = time_plot.line(t, np.zeros(len(t)), color="red", line_width=1) # set x values but use dummy values for y
 
     # Waterfall Sink ("image" plot)
-    waterfall_plot = figure(plot_width=300, # this is more for the ratio, because we have auto-width scaling
-                            plot_height=200,
-                            x_range=(0, fft_size), 
-                            y_range=(0, waterfall_samples),
-                            y_axis_label='Time',
-                            x_axis_label='',
-                            tools=[BoxZoomTool(), ResetTool(), SaveTool()],
-                            title="Waterfall")
-    waterfall_plot.toolbar.logo=None # hides stupid bokeh logo
+    waterfall_plot = pysdr.base_plot(' ', 'Time', 'Waterfall', disable_all_zooming=True) 
+    waterfall_plot._set_x_range(0, fft_size) # Bokeh tries to automatically figure out range, but in this case we need to specify it
+    waterfall_plot._set_y_range(0, waterfall_samples)
     waterfall_plot.axis.visible = False # i couldn't figure out how to update x axis when freq changes, so just hide them for now
-    waterfall_data = waterfall_plot.image(image=[np.ones((waterfall_samples, fft_size))*-100.0],  # input has to be in list form
-                                          x=0, 
-                                          y=0, 
-                                          dw=fft_size, 
-                                          dh=waterfall_samples, 
-                                          palette="Spectral9") # closest thing to matlab's jet    
+    waterfall_data = waterfall_plot.image(image = [shared_buffer['waterfall']],  # input has to be in list form
+                                          x = 0, # start of x
+                                          y = 0, # start of y
+                                          dw = fft_size, # size of x
+                                          dh = waterfall_samples, # size of y
+                                          palette = "Spectral9") # closest thing to matlab's jet    
 
     # IQ/Constellation Sink ("circle" plot)
-    iq_plot = figure(plot_width=300, # this is more for the ratio, because we have auto-width scaling
-                     plot_height=200,
-                     x_range=(-1.0, 1.0), # amplitude 
-                     y_range=(-1.0, 1.0), # amplitude 
-                     tools=[BoxZoomTool(), WheelZoomTool(), ResetTool(), SaveTool()],
-                     title="IQ Plot")
-    iq_plot.toolbar.active_scroll = iq_plot.toolbar.tools[1] # sets wheel zoom active by default (tools[1] is the wheelzoom)
-    iq_plot.toolbar.logo=None # hides stupid bokeh logo
-    samples_to_plot = fft_size # to make it easier
-    iq_data = iq_plot.circle(range(samples_to_plot), 
-                                range(samples_to_plot),
-                                line_alpha=0.0, # setting line_width=0 didn't make it go away 
-                                fill_color="aqua",
-                                fill_alpha=0.5,
-                                size=4)
+    iq_plot = pysdr.base_plot(' ', ' ', 'IQ Plot')
+    iq_plot._set_x_range(-1.0, 1.0) # this is to keep it fixed at -1 to 1. you can also just zoom out with mouse wheel and it will stop auto-ranging
+    iq_plot._set_y_range(-1.0, 1.0)
+    iq_data = iq_plot.circle(np.zeros(samples_in_time_plots), 
+                             np.zeros(samples_in_time_plots),
+                             line_alpha=0.0, # setting line_width=0 didn't make it go away, but this works
+                             fill_color="aqua",
+                             fill_alpha=0.5, 
+                             size=4) # size of circles
 
     def gain_callback(attr, old, new):
         shared_buffer['stop-signal'] = True # triggers a stop of the asynchronous read (cant change gain during it)
