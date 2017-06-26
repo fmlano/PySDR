bb66ffe10f9c783648040ce5de952ba99ac362a3
Marc L <marcll@vt.edu>
2017-06-26 00:16:01 -0400

    created non-flask (just normal Bokeh) versions of such samples and rtl_demo

---
+++ rtl_demo_nonflask.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+# to run this app use: bokeh serve rtl_demo_nonweb.py --show
+
+import pysdr # our python package
+
+import numpy as np
+import time 
+from scipy.signal import firwin # FIR filter design using the window method
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row, gridplot, Spacer, widgetbox
+from bokeh.models import Select, TextInput
+from bokeh.util.browser import view # utility to Open a browser to view the specified location.
+
+from rtlsdr import RtlSdr
+
+from multiprocessing import Process, Manager 
+
+# Parameters
+fft_size = 512               # output size of fft, the input size is the samples_per_batch
+waterfall_samples = 100      # number of rows of the waterfall
+samples_per_batch = 256*1024 # num of samples that we process at a time
+samples_in_time_plots = 500  # should be less than samples_per_batch
+
+# RTL-SDR stuff
+sdr = RtlSdr()
+sdr.sample_rate = 2.048e6  # Hz
+sdr.center_freq = 101.1e6  # Hz
+sdr.freq_correction = 60   # PPM
+sdr.gain = sdr.get_gains()[-1]/10.0 # highest gain to start with
+
+# Set up the shared buffer between threads (using multiprocessing's Manager).  it is global
+manager = Manager()
+shared_buffer = manager.dict() # there is also an option to use a list
+shared_buffer['waterfall'] = np.ones((waterfall_samples, fft_size))*-100.0 # waterfall buffer
+shared_buffer['psd'] = np.zeros(fft_size) # PSD buffer
+shared_buffer['i'] = np.zeros(samples_in_time_plots) # I buffer (time domain)
+shared_buffer['q'] = np.zeros(samples_in_time_plots) # Q buffer (time domain)
+shared_buffer['stop-signal'] = False # used to signal RTL to stop (when it goes true)
+shared_buffer['utilization'] = 0.0 # float between 0 and 1, used to store how the process_samples is keeping up
+
+# create a streaming-type FIR filter (this should act the same as a FIR filter block in GNU Radio)
+taps = firwin(numtaps=100, cutoff=500e3, nyq=sdr.sample_rate)
+prefilter = pysdr.fir_filter(taps)
+
+# Function that processes each batch of samples that comes in (currently, all DSP goes here)
+def process_samples(samples, rtlsdr_obj):
+    startTime = time.time()
+    samples = prefilter.filter(samples)
+    PSD = 10.0 * np.log10(np.abs(np.fft.fftshift(np.fft.fft(samples, fft_size)/float(fft_size)))**2) # calcs PSD
+    waterfall = shared_buffer['waterfall'] # pull waterfall from buffer
+    waterfall[:] = np.roll(waterfall, -1, axis=0) # shifts waterfall 1 row
+    waterfall[-1,:] = PSD # fill last row with new fft results
+    shared_buffer['waterfall'] = waterfall # you have to copy it back into the manager_list
+    shared_buffer['psd'] = PSD # overwrites whatever was in psd buffer, so that the GUI uses the most recent one when it goes to refresh itself
+    shared_buffer['i'] = np.real(samples[0:samples_in_time_plots]) # i buffer
+    shared_buffer['q'] = np.imag(samples[0:samples_in_time_plots]) # q buffer
+    # if the change-gain or change-freq callback function signaled STOP then we need to cancel the async read
+    if shared_buffer['stop-signal'] == True:
+        sdr.cancel_read_async() # needs to be called from this function, so we use the shared memory to send a signal
+    shared_buffer['utilization'] = (time.time() - startTime)/float(samples_per_batch)*sdr.sample_rate # should be below 1.0 to avoid overflows
+
+# Function that runs asynchronous reading from the RTL, and is a blocking function
+def start_sdr():
+    while True:
+        if shared_buffer['stop-signal'] == False:
+            sdr.read_samples_async(callback=process_samples, num_samples=samples_per_batch) # sets RTL to asynchronous mode using the specified callback
+        time.sleep(0.1) # we only wait here when stop-signal == True, which is usually very brief
+        
+# Start SDR sample processign as a separate thread
+p = Process(target=start_sdr) 
+p.start()
+
+
+# Frequncy Sink (line plot)
+fft_plot = pysdr.base_plot('Freq [MHz]', 'PSD [dB]', 'Frequency Sink', disable_horizontal_zooming=True) 
+f = (np.linspace(-sdr.sample_rate/2.0, sdr.sample_rate/2.0, fft_size) + sdr.center_freq)/1e6
+fft_line = fft_plot.line(f, np.zeros(len(f)), color="aqua", line_width=1) # set x values but use dummy values for y
+
+# Time Sink (line plot)
+time_plot = pysdr.base_plot('Time [ms]', ' ', 'Time Sink', disable_horizontal_zooming=True) 
+t = np.linspace(0.0, samples_in_time_plots / sdr.sample_rate, samples_in_time_plots) * 1e3 # in ms
+timeI_line = time_plot.line(t, np.zeros(len(t)), color="aqua", line_width=1) # set x values but use dummy values for y
+timeQ_line = time_plot.line(t, np.zeros(len(t)), color="red", line_width=1) # set x values but use dummy values for y
+
+# Waterfall Sink ("image" plot)
+waterfall_plot = pysdr.base_plot(' ', 'Time', 'Waterfall', disable_all_zooming=True) 
+waterfall_plot._set_x_range(0, fft_size) # Bokeh tries to automatically figure out range, but in this case we need to specify it
+waterfall_plot._set_y_range(0, waterfall_samples)
+waterfall_plot.axis.visible = False # i couldn't figure out how to update x axis when freq changes, so just hide them for now
+waterfall_data = waterfall_plot.image(image = [shared_buffer['waterfall']],  # input has to be in list form
+                                      x = 0, # start of x
+                                      y = 0, # start of y
+                                      dw = fft_size, # size of x
+                                      dh = waterfall_samples, # size of y
+                                      palette = "Spectral9") # closest thing to matlab's jet    
+
+# IQ/Constellation Sink ("circle" plot)
+iq_plot = pysdr.base_plot(' ', ' ', 'IQ Plot')
+iq_plot._set_x_range(-1.0, 1.0) # this is to keep it fixed at -1 to 1. you can also just zoom out with mouse wheel and it will stop auto-ranging
+iq_plot._set_y_range(-1.0, 1.0)
+iq_data = iq_plot.circle(np.zeros(samples_in_time_plots), 
+                         np.zeros(samples_in_time_plots),
+                         line_alpha=0.0, # setting line_width=0 didn't make it go away, but this works
+                         fill_color="aqua",
+                         fill_alpha=0.5, 
+                         size=4) # size of circles
+
+# Utilization bar (standard plot defined in gui.py)
+utilization_plot = pysdr.utilization_bar(1.0) # sets the top at 10% instead of 100% so we can see it move
+utilization_data = utilization_plot.quad(top=[shared_buffer['utilization']], bottom=[0], left=[0], right=[1], color="#B3DE69") #adds 1 rectangle
+
+def gain_callback(attr, old, new):
+    shared_buffer['stop-signal'] = True # triggers a stop of the asynchronous read (cant change gain during it)
+    time.sleep(0.5) # give time for the stop signal to trigger it- if you get a segfault then this needs to be increased
+    sdr.gain = float(new) # set new gain
+    shared_buffer['stop-signal'] = False  # turns off "stop" signal
+
+def freq_callback(attr, old, new):
+    shared_buffer['stop-signal'] = True # see above comments
+    time.sleep(0.5)
+    sdr.center_freq = float(new) # TextInput provides a string
+    f = np.linspace(-sdr.sample_rate/2.0, sdr.sample_rate/2.0, fft_size) + sdr.center_freq
+    fft_line.data_source.data['x'] = f/1e6 # update x axis of freq sink
+    shared_buffer['stop-signal'] = False
+    
+# gain selector
+gain_select = Select(title="Gain:", value=str(sdr.gain), options=[str(i/10.0) for i in sdr.get_gains()])
+gain_select.on_change('value', gain_callback)
+
+# center_freq TextInput
+freq_input = TextInput(value=str(sdr.center_freq), title="Center Freq [Hz]")
+freq_input.on_change('value', freq_callback)
+
+# add the widgets to the document
+curdoc().add_root(row([widgetbox(gain_select, freq_input), utilization_plot])) # widgetbox() makes them a bit tighter grouped than column()
+
+# Add four plots to document, using the gridplot method of arranging them
+curdoc().add_root(gridplot([[fft_plot, time_plot], [waterfall_plot, iq_plot]], sizing_mode="scale_width", merge_tools=False)) # Spacer(width=20, sizing_mode="fixed")
+
+
+# This function gets called periodically, and is how the "real-time streaming mode" works   
+def plot_update():  
+    timeI_line.data_source.data['y'] = shared_buffer['i'] # send most recent I to time sink
+    timeQ_line.data_source.data['y'] = shared_buffer['q'] # send most recent Q to time sink
+    iq_data.data_source.data['x'] = shared_buffer['i'] # send most recent I to IQ
+    iq_data.data_source.data['y'] = shared_buffer['q'] # send most recent Q to IQ
+    fft_line.data_source.data['y'] = shared_buffer['psd'] # send most recent psd to freq sink
+    waterfall_data.data_source.data['image'] = [shared_buffer['waterfall']] # send waterfall 2d array to waterfall sink
+    utilization_data.data_source.data['top'] = [shared_buffer['utilization']] # send most recent utilization level (only need to adjust top of rectangle)
+
+# Add a periodic callback to be run every x milliseconds
+curdoc().add_periodic_callback(plot_update, 150) 
+
+# pull out a theme from themes.py
+#curdoc().theme = pysdr.black_and_white
+             
+
+
+
+    
+    
---
+++ such_samples_clone_nonflask.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+# to run this app use: bokeh serve such_samples_clone_nonweb.py --show
+
+import pysdr # our python package
+
+import numpy as np
+import time 
+import os
+
+from bokeh.io import curdoc
+from bokeh.application import Application
+from bokeh.layouts import column, row, gridplot, Spacer, widgetbox
+from bokeh.models import Select, TextInput, Slider
+
+# Parameters
+iq_file = '/home/marc/fm-recording.iq'  # for testing I created one using standard file sink in gnuradio
+sample_rate = 10e6  # what did you use for recording it?
+center_freq = 100e6 # what did you use for recording it?
+current_window_size = 50000 # starting window size
+fft_size = 128      # output size of fft, the input size is the samples_per_batch
+decimation_factor = 500 # amount to decimate so we aren't displaying millions of points on a plot (also acts as the limit)
+
+# Globals
+current_start_sample = 0 # this can be changed with the slider
+total_samples = os.path.getsize(iq_file)/8
+waterfall_samples = int(current_window_size/fft_size) # number of rows of the waterfall
+
+
+# Frequncy Sink (line plot)
+fft_plot = pysdr.base_plot('Freq [MHz]', 'PSD [dB]', 'Frequency Sink', disable_horizontal_zooming=True) 
+f = (np.linspace(-sample_rate/2.0, sample_rate/2.0, fft_size) + center_freq)/1e6
+fft_line = fft_plot.line(f, np.zeros(len(f)), color="aqua", line_width=1) # set x values but use dummy values for y
+
+# Time Sink (line plot)
+time_plot = pysdr.base_plot('Time [ms]', 'Amplitude', 'Time Sink', disable_horizontal_zooming=True) 
+decimation = int(current_window_size/decimation_factor) # so we only display decimation_factor points at most
+if decimation > 1:
+    t = np.linspace(0.0, current_window_size / sample_rate, current_window_size/decimation) * 1e3 # in ms
+else:
+    t = np.linspace(0.0, current_window_size / sample_rate, current_window_size) * 1e3 # in ms
+timeI_line = time_plot.line(t, np.zeros(len(t)), color="aqua", line_width=1) # set x values but use dummy values for y
+timeQ_line = time_plot.line(t, np.zeros(len(t)), color="red", line_width=1) # set x values but use dummy values for y
+
+# Waterfall Sink ("image" plot)
+waterfall_plot = pysdr.base_plot('Freq [MHz]', 'Time [ms]', 'Waterfall', plot_height=400, disable_all_zooming=True) 
+waterfall_plot._set_x_range(f[0], f[-1]) 
+waterfall_plot._set_y_range(0, t[-1])
+waterfall_plot.yaxis.visible = False # i couldn't figure out how to update y axis when time scale changes, so just hide for now
+waterfall_data = waterfall_plot.image(image = [np.zeros((waterfall_samples, fft_size))],  # input has to be in list form
+                                      x = f[0], # start of x
+                                      y = 0, # start of y
+                                      dw = f[-1] - f[0], # size of x
+                                      dh = t[-1], # size of y
+                                      palette = "Spectral9") # closest thing to matlab's jet    
+
+def process_samples():
+    # read a portion of the file
+    with open(iq_file) as fin:
+        fin.seek(current_start_sample*8) # 8 bytes per complex sample
+        samples = np.fromfile(fin, dtype=np.complex64, count = current_window_size)
+    
+    # DSP, and store results to buffers
+    PSD = 10.0 * np.log10(np.abs(np.fft.fftshift(np.fft.fft(samples, fft_size)/float(fft_size)))**2) # calcs PSD
+    decimation = int(current_window_size/decimation_factor)
+    if decimation > 1:
+        i = np.real(samples[::decimation]) 
+        q = np.imag(samples[::decimation]) 
+    else:
+        i = np.real(samples) 
+        q = np.imag(samples) 
+
+    # calc waterfall
+    waterfall = np.zeros((waterfall_samples, fft_size))
+    for waterfall_row in range(waterfall_samples):
+        waterfall[waterfall_row,:] = 10.0 * np.log10(np.abs(np.fft.fftshift(np.fft.fft(samples[waterfall_row*fft_size:(waterfall_row+1)*fft_size])/float(fft_size)))**2)
+    # send results to GUI
+    timeI_line.data_source.data['y'] = i # send most recent I to time sink
+    timeQ_line.data_source.data['y'] = q # send most recent Q to time sink
+    fft_line.data_source.data['y'] = PSD # send most recent psd to freq sink
+    waterfall_data.data_source.data['image'] = [waterfall] # send waterfall 2d array to waterfall sink
+
+def position_callback(attr, old, new):
+    global current_start_sample # any variable we plan to change needs to be declared global
+    current_start_sample = new
+    process_samples() # reprocess samples
+
+def window_size_callback(attr, old, new):
+    global current_window_size
+    global waterfall_samples
+    current_window_size = int(new) # TextInput provides a string
+    waterfall_samples = int(current_window_size/fft_size) # number of rows of the waterfall
+    # update x axis of time sink and waterfall
+    decimation = int(current_window_size/decimation_factor) 
+    if decimation > 1:
+        t = np.linspace(0.0, current_window_size / sample_rate, current_window_size/decimation) * 1e3 # in ms
+    else:
+        t = np.linspace(0.0, current_window_size / sample_rate, current_window_size) * 1e3 # in ms
+    timeI_line.data_source.data['x'] = t
+    timeQ_line.data_source.data['x'] = t
+    process_samples() # reprocess since we changed the window size
+
+# position slider
+position_slider = Slider(start=0, end=(total_samples-current_window_size), value=0, step=100, title="Start Sample") #FIXME end is not correct
+position_slider.on_change('value', position_callback)
+
+# center_freq TextInput
+window_size_input = TextInput(value=str(current_window_size), title="Window Size") # FIXME add limits
+window_size_input.on_change('value', window_size_callback)
+
+# add the widgets to the document
+curdoc().add_root(widgetbox(position_slider, window_size_input)) # widgetbox() makes them a bit tighter grouped than column()
+
+# assemble the plots the way we like to, and add to document
+col1 = column([waterfall_plot], sizing_mode="scale_width")
+col2 = column([time_plot, fft_plot], sizing_mode="scale_width")
+row1 = row([col1, col2], sizing_mode="scale_width")
+curdoc().add_root(row1)
+
+# pull out a theme from themes.py
+#curdoc().theme = pysdr.black_and_white
+
+print "Starting!"
+process_samples() # go ahead and process samples using the initial parameters (starting at sample 0)
+
+
+
+             
+
+
+    
